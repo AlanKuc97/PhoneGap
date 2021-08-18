c914cc688073344f239bcfaae40b54f67cc4c7e5
Alan Kuchinskij <alankuchinskij@Alans-MBP.lan>
2021-08-18 15:48:05 +0300

    Created few functions, changed obsticle height

--- MyApp/www/JavaScript/gamePlay.js
+++ MyApp/www/JavaScript/gamePlay.js
@@ -1,20 +1,50 @@
 // TODO: 
-  // 1. Correct player speed on mobile phone +- Done
-  // 2. Create function for gameover.. DRY 
+  // 1. Add more small obsticles
 class GamePlay extends Phaser.Scene{
 	constructor(){
 		super({key : 'gamePlay'});
 	}
 	preload(){
 	}
-	
+	gameOverCollaps(rectY, playerY){
+		if (rectY <= playerY){
+			if(GamePlay.score > parseInt(localStorage.getItem("makeItHighScore"))){
+				localStorage.setItem("makeItHighScore", GamePlay.score);
+			}
+			this.scene.start('gameOver');
+		}
+	}
+	gameScore(){
+		if(GamePlay.redRect[0].y > config.height/2 && GamePlay.redScored == 0){
+			GamePlay.redScored = 1;
+			GamePlay.score += 10;
+			GamePlay.scoreText.setText('Score: ' + GamePlay.score);
+		}else if(GamePlay.blueRect[0].y > config.height/2 && GamePlay.blueScored == 0){
+			GamePlay.blueScored = 1;
+			GamePlay.score += 10;
+			GamePlay.scoreText.setText('Score: ' + GamePlay.score);
+		}else if(GamePlay.greenRect[0].y > config.height/2 && GamePlay.greenScored == 0){
+			GamePlay.greenScored = 1;
+			GamePlay.score += 10;
+			GamePlay.scoreText.setText('Score: ' + GamePlay.score);
+		}
+	}
+	gameDownObsticles(rectY){
+		GamePlay.redRect[0].y += rectY;
+		GamePlay.redRect[1].y += rectY;
+		GamePlay.blueRect[0].y += rectY;
+		GamePlay.blueRect[1].y += rectY ;
+		GamePlay.greenRect[0].y += rectY;
+		GamePlay.greenRect[1].y += rectY;
+	}
+
 	create(){
 		//Creating player, setting display size and interactive
 		//GamePlay.player = new Phaser.Geom.Rectangle(config.width / 2,(config.height / 2)+100,16,16);
 		GamePlay.player = this.add.rectangle(config.width / 2,(config.height / 2)+100,16,16, 0x000000);
 		GamePlay.playerRotation = 0;
 		GamePlay.spaceBetween = config.width / 100 * 30;
-		GamePlay.obsticleHeight = 20;
+		GamePlay.obsticleHeight = 40;
 		GamePlay.minVisibleObsticle = 10;
 		GamePlay.MAX = 0 - GamePlay.spaceBetween - GamePlay.minVisibleObsticle;
 		GamePlay.MIN = GamePlay.minVisibleObsticle - config.width;
@@ -48,7 +78,7 @@ class GamePlay extends Phaser.Scene{
 						  new Phaser.Geom.Rectangle(550,-585,config.width,GamePlay.obsticleHeight)];
 		GamePlay.greenRect[1].x = GamePlay.greenRect[0].x + GamePlay.greenRect[0].width + GamePlay.spaceBetween;
 		//Graphics config
-		GamePlay.graphics = this.add.graphics({fillStyle: { color: 0xff0000} });
+		GamePlay.graphics = this.add.graphics({fillStyle: { color: 0xAF1A12} });
 		//GamePlay.graphics.fillRectShape(GamePlay.redRect[0]);
 		//GamePlay.graphics.fillRectShape(GamePlay.redRect[1]);
 		
@@ -60,9 +90,10 @@ class GamePlay extends Phaser.Scene{
 		GamePlay.redScored = 0;
 		GamePlay.blueScored = 0;
 		GamePlay.greenScored = 0;
+
 	}
+
 	update(){
-		console.log('GamePlay.rect: ' + GamePlay.rectY);
 		GamePlay.player.angle += GamePlay.playerRotation;
 		//Clearing graphics data 
 		GamePlay.graphics.clear();
@@ -78,29 +109,13 @@ class GamePlay extends Phaser.Scene{
 		if (GamePlay.player.y > config.height/2){
 			GamePlay.player.y += GamePlay.rectY;
 			if(GamePlay.player.y < config.height/2 + 50 && GamePlay.rectY > 0){
-				GamePlay.redRect[0].y += GamePlay.rectY;
-				GamePlay.redRect[1].y += GamePlay.rectY;
-				GamePlay.blueRect[0].y += GamePlay.rectY;
-				GamePlay.blueRect[1].y += GamePlay.rectY ;
-				GamePlay.greenRect[0].y += GamePlay.rectY;
-			    GamePlay.greenRect[1].y += GamePlay.rectY;
-			    //console.log('GamePlay.rectY: ' + GamePlay.rectY);
+			 	this.gameDownObsticles(GamePlay.rectY);
 			}
 		}else if (GamePlay.player.y <= config.height/2 && GamePlay.rectY > 0){
 			GamePlay.player.y += GamePlay.rectY;
-			GamePlay.redRect[0].y += GamePlay.rectY;
-			GamePlay.redRect[1].y += GamePlay.rectY;
-			GamePlay.blueRect[0].y += GamePlay.rectY;
-			GamePlay.blueRect[1].y += GamePlay.rectY;
-			GamePlay.greenRect[0].y += GamePlay.rectY;
-		    GamePlay.greenRect[1].y += GamePlay.rectY;
+		 	this.gameDownObsticles(GamePlay.rectY);
 		}else{
-			GamePlay.redRect[0].y -= GamePlay.rectY;
-			GamePlay.redRect[1].y -= GamePlay.rectY;
-			GamePlay.blueRect[0].y -= GamePlay.rectY;
-			GamePlay.blueRect[1].y -= GamePlay.rectY;
-			GamePlay.greenRect[0].y -= GamePlay.rectY;
-		    GamePlay.greenRect[1].y -= GamePlay.rectY;
+		    this.gameDownObsticles(-GamePlay.rectY);
 		}
 		//Player direction
 		if (GamePlay.rectX > 0) {
@@ -115,26 +130,15 @@ class GamePlay extends Phaser.Scene{
 		//Fill
 		GamePlay.graphics.fillRectShape(GamePlay.redRect[0]);
 		GamePlay.graphics.fillRectShape(GamePlay.redRect[1]);
-		GamePlay.graphics.fillStyle(0x0000ff); 
+		GamePlay.graphics.fillStyle(0x0D71FD); 
 		GamePlay.graphics.fillRectShape(GamePlay.blueRect[0]);
 		GamePlay.graphics.fillRectShape(GamePlay.blueRect[1]);
-		GamePlay.graphics.fillStyle(0x00ff00);
+		GamePlay.graphics.fillStyle(0x1A841A);
 		GamePlay.graphics.fillRectShape(GamePlay.greenRect[0]);
 		GamePlay.graphics.fillRectShape(GamePlay.greenRect[1]);
-		if(GamePlay.redRect[0].y > config.height/2 && GamePlay.redScored == 0){
-			GamePlay.redScored = 1;
-			GamePlay.score += 10;
-			GamePlay.scoreText.setText('Score: ' + GamePlay.score);
-		}else if(GamePlay.blueRect[0].y > config.height/2 && GamePlay.blueScored == 0){
-			GamePlay.blueScored = 1;
-			GamePlay.score += 10;
-			GamePlay.scoreText.setText('Score: ' + GamePlay.score);
-		}else if(GamePlay.greenRect[0].y > config.height/2 && GamePlay.greenScored == 0){
-			GamePlay.greenScored = 1;
-			GamePlay.score += 10;
-			GamePlay.scoreText.setText('Score: ' + GamePlay.score);
-		}
-
+		
+		//Check scored
+		this.gameScore();
 
 		//Rectangle Loop
 		if(GamePlay.redRect[0].y > config.height){
@@ -160,53 +164,23 @@ class GamePlay extends Phaser.Scene{
 
 		//Left Rectangle collapse
 		if(GamePlay.player.y <= GamePlay.redRect[0].y+GamePlay.redRect[0].height && GamePlay.player.x <= GamePlay.redRect[0].x + GamePlay.redRect[0].width){
-			if (GamePlay.redRect[0].y <= GamePlay.player.y){
-				if(GamePlay.score > parseInt(localStorage.getItem("makeItHighScore"))){
-					localStorage.setItem("makeItHighScore", GamePlay.score);
-				}
-				this.scene.start('gameOver');
-			}
+			this.gameOverCollaps(GamePlay.redRect[0].y,GamePlay.player.y);
 		}
 		if(GamePlay.player.y <= GamePlay.blueRect[0].y+GamePlay.blueRect[0].height && GamePlay.player.x <= GamePlay.blueRect[0].x + GamePlay.blueRect[0].width){
-			if (GamePlay.blueRect[0].y <= GamePlay.player.y){
-				if(GamePlay.score > parseInt(localStorage.getItem("makeItHighScore"))){
-					localStorage.setItem("makeItHighScore", GamePlay.score);
-				}
-				this.scene.start('gameOver');
-			}
+			this.gameOverCollaps(GamePlay.blueRect[0].y,GamePlay.player.y);
 		}
 		if(GamePlay.player.y <= GamePlay.greenRect[0].y+GamePlay.greenRect[0].height && GamePlay.player.x <= GamePlay.greenRect[0].x + GamePlay.greenRect[0].width){
-			if (GamePlay.greenRect[0].y <= GamePlay.player.y){
-				if(GamePlay.score > parseInt(localStorage.getItem("makeItHighScore"))){
-					localStorage.setItem("makeItHighScore", GamePlay.score);
-				}
-				this.scene.start('gameOver');
-			}
+			this.gameOverCollaps(GamePlay.greenRect[0].y,GamePlay.player.y);
 		}
 		//Right Rectangle collapse
 		if(GamePlay.player.y <= GamePlay.redRect[1].y+GamePlay.redRect[1].height && GamePlay.player.x >= GamePlay.redRect[1].x){
-			if (GamePlay.redRect[1].y <= GamePlay.player.y){
-				if(GamePlay.score > parseInt(localStorage.getItem("makeItHighScore"))){
-					localStorage.setItem("makeItHighScore", GamePlay.score);
-				}
-				this.scene.start('gameOver');
-			}
+			this.gameOverCollaps(GamePlay.redRect[1].y,GamePlay.player.y);
 		}
 		if(GamePlay.player.y <= GamePlay.blueRect[1].y+GamePlay.blueRect[1].height && GamePlay.player.x >= GamePlay.blueRect[1].x){
-			if (GamePlay.blueRect[1].y <= GamePlay.player.y){
-				if(GamePlay.score > parseInt(localStorage.getItem("makeItHighScore"))){
-					localStorage.setItem("makeItHighScore", GamePlay.score);
-				}
-				this.scene.start('gameOver');
-			}
+			this.gameOverCollaps(GamePlay.blueRect[1].y,GamePlay.player.y);
 		}
 		if(GamePlay.player.y <= GamePlay.greenRect[1].y+GamePlay.greenRect[1].height && GamePlay.player.x >= GamePlay.greenRect[1].x){
-			if (GamePlay.greenRect[1].y < GamePlay.player.y){
-				if(GamePlay.score > parseInt(localStorage.getItem("makeItHighScore"))){
-					localStorage.setItem("makeItHighScore", GamePlay.score);
-				}
-				this.scene.start('gameOver');
-			}
+			this.gameOverCollaps(GamePlay.greenRect[1].y,GamePlay.player.y);
 		}
 		// End game 
 		if (GamePlay.player.y > config.height){
@@ -225,31 +199,28 @@ class GamePlay extends Phaser.Scene{
  		});
  		//Movement
  		if(GamePlay.counter > 0 && GamePlay.counter < GamePlay.maxCounerValue/2){
- 			//console.log('I am in first if');
  			GamePlay.rectX = GamePlay.xStep;
 			GamePlay.rectY = GamePlay.yStep;
-			GamePlay.playerRotation = 1;
+			GamePlay.playerRotation = 2;
 			GamePlay.counter += 1;
  		}else if(GamePlay.counter > 0 && GamePlay.counter < GamePlay.maxCounerValue){
  			GamePlay.rectX = GamePlay.xStep;
- 			GamePlay.playerRotation = 1;
+ 			GamePlay.playerRotation = 2;
 			GamePlay.counter += 1;
  		}else if(GamePlay.counter < 0 && 
  				 GamePlay.counter > (GamePlay.maxCounerValue - (GamePlay.maxCounerValue * 2))/2){
- 			//console.log('I am in second if');
- 			GamePlay.rectX =  0 - GamePlay.xStep;
+ 			GamePlay.rectX = -GamePlay.xStep;
 			GamePlay.rectY = GamePlay.yStep;
-			GamePlay.playerRotation = -1;
+			GamePlay.playerRotation = -2;
 			GamePlay.counter -= 1;
  		}else if(GamePlay.counter < 0 && 
  				 GamePlay.counter > (GamePlay.maxCounerValue - (GamePlay.maxCounerValue * 2))){
- 			GamePlay.rectX = 0 - GamePlay.xStep;
- 			GamePlay.playerRotation = -1;
+ 			GamePlay.rectX = -GamePlay.xStep;
+ 			GamePlay.playerRotation = -2;
 			GamePlay.counter -= 1;
  		}else{
  			GamePlay.counter = 0;
  		}
- 		//console.log('GamePlay.counter: ' + (GamePlay.maxCounerValue - (GamePlay.maxCounerValue * 2))/2);
 	}
 
 }
